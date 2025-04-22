9bc60a4fc72a3a1d080291e632e8deb47e9f618a
Pedro Lins <104699854+Ricadros@users.noreply.github.com>
2025-04-22 08:52:39 -0400

    Delete Questao 07.py

--- Questao 07.py
+++
@@ -1,8 +0,0 @@
-#07 - Faça um Programa que leia três números e mostre o maior deles.
-
-numero1 = int(input('Digite o primeiro número'))
-numero2 = int(input('Digite o segundo número'))
-numero3 = int(input('Digite o terceiro número'))
-
-numero4 = max(numero1,numero2,numero3)
-print('O MAIOR NÚMERO É:',numero4)
