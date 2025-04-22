61a757abb8d02536a31aa67be04981f35090197c
Pedro Lins <104699854+Ricadros@users.noreply.github.com>
2025-04-22 10:11:05 -0400

    Add files via upload

---
+++ LISTA1/Questao02.py
@@ -0,0 +1,8 @@
+#2 - Faça um Programa que peça dois números e imprima a soma.
+
+numero01 = int(input('DIGITE SEU PRIMEIRO NUMERO:'))
+numero02 = int(input('DIGITE SEU SEGUNDO NUMERO'))
+
+soma = numero01 + numero02 
+
+print('O RESULTADO DA SUA SOMA É :',soma)
