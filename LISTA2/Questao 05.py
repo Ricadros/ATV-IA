0df209d1c605cc62ff514e8abe210346ba310e03
Pedro Lins <104699854+Ricadros@users.noreply.github.com>
2025-04-22 11:06:08 -0400

    Delete LISTA2/Questao 05.py

--- LISTA2/Questao 05.py
+++
@@ -1,7 +0,0 @@
-#05 - Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
-
-letra = input('Digite uma letra')
-if letra in 'aeiou' :
-    print('é uma vogal')
-else:
-    print('é uma consoante')
