6ae03d4a57ccaaa9b8ca61d0533973f0317e3900
Pedro Lins <104699854+Ricadros@users.noreply.github.com>
2025-04-22 11:06:47 -0400

    Add files via upload

---
+++ LISTA2/Questao04.py
@@ -0,0 +1,7 @@
+#4 - Faça um Programa que converta metros para centímetros.
+
+metros = int(input('DIGITE QUANTOS METROS VOCE QUER CONVERTER'))
+
+cm = metros*100
+
+print('Voce precisa de',cm,'cm')
---
+++ LISTA2/Questao05.py
@@ -0,0 +1,6 @@
+#5 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+diametro = float(input('DIGITE O DIAMETRO DO CIRCULO'))
+
+raio = diametro/2
+print('O RAIO DESTE CIRCULO É:',raio)
