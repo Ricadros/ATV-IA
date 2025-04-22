c3704bd8f3abc3614a5b162c0f961ecb11398b0f
Pedro Lins <104699854+Ricadros@users.noreply.github.com>
2025-04-22 10:34:40 -0400

    Add files via upload

---
+++ LISTA1/Questao03.py
@@ -0,0 +1,10 @@
+#3 - Faça um Programa que peça as 4 notas bimestrais e mostre a média. 
+
+nota1 = int(input('DIGITE A SUA NOTA DO PRIMEIRO BIMESTRE'))
+nota2 = int(input('DIGITE A SUA NOTA DO SEGUNDO BIMESTRE'))
+nota3 = int(input('DIGITE A SUA NOTA DO TERCEIRO BIMESTRE'))
+nota4 = int(input('DIGITE A SUA NOTA DO QUARTO BIMESTRE'))
+
+media = (nota1 + nota2 +nota3 + nota4) /4
+
+print('Sua média final foi de:',media)
---
+++ LISTA1/Questao06.py
@@ -0,0 +1,10 @@
+#6 - Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+altura = float(input('DIGITE A ALTURA DO QUADRADO'))
+base = float(input('DIGITE A BASE DO QUADRADO'))
+
+area = altura*base 
+
+dobro = area*2 
+
+print('o dobro da area desse quadrado é',dobro)
