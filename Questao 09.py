07bfa85f1bbbcc796c9fccac21e9c6964f856636
Pedro Lins <104699854+Ricadros@users.noreply.github.com>
2025-04-22 08:52:56 -0400

    Delete Questao 09.py

--- Questao 09.py
+++
@@ -1,8 +0,0 @@
-#09 - Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
-
-produto1 = float(input('DIGITE O VALOR DO PRIMEIRO PRODUTO'))
-produto2 = float(input('DIGITE O VALOR DO SEGUNDO PRODUTO'))
-produto3 = float(input('DIGITE O VALOR DO TERCEIRO PRODUTO'))
-
-valor = min(produto1,produto2,produto3)
-print('Voce deve optar pelo produto',valor,'pelo seu custo beneficio, é o que mais vale a pena')
