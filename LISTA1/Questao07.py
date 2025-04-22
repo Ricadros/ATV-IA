e5d0dafeb85edd59e937b35c8118420b5f2bff33
Pedro Lins <104699854+Ricadros@users.noreply.github.com>
2025-04-22 10:44:47 -0400

    Add files via upload

---
+++ LISTA1/Questao07.py
@@ -0,0 +1,8 @@
+#7 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
+ganho = float(input('QUANTO VOCE GANHA POR HORA?'))
+horas = int(input('QUANTAS HORAS VOCE TRABALHA NO MES?'))
+
+salario = ganho * horas
+
+print('O seu salario mensal é de',salario)
