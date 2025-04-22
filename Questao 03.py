0cb18fb6c0893a908bbe46c9cf32aafebe1eae20
Pedro Lins <104699854+Ricadros@users.noreply.github.com>
2025-04-22 08:51:29 -0400

    Delete Questao 03.py

--- Questao 03.py
+++
@@ -1,9 +0,0 @@
-#03 - Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
-
-num = int(input('Digite o número'))
-
-if num > 0 : 
-    print('este número é positivo!!')
-else :
-    print('Este número é negativo')
-    
