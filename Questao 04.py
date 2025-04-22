778cbe2eb0b1dea87fc1e481a59e4fb20c91f86e
Pedro Lins <104699854+Ricadros@users.noreply.github.com>
2025-04-22 08:52:06 -0400

    Delete Questao 04.py

--- Questao 04.py
+++
@@ -1,6 +0,0 @@
-#04 - Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
-
-sexo = str(input('Digite Seu SEXO = F OU M '))
-if sexo == 'F' :
-    print('Sexo feminino, sexo ACEITO')
-else : print('Sexo masculino, sexo inváldo')
