5a3c156da842c58f6b33aa3383418f6390de6fc6
Pedro Lins <104699854+Ricadros@users.noreply.github.com>
2025-04-23 12:46:16 -0400

    Add files via upload

---
+++ LISTA1/Questao10.py
@@ -0,0 +1,8 @@
+#10 - Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre: 
+n1 = int(input('Digite o primeiro número'))
+n2 = int(input('Digite o segundo numero'))
+n3 = float(input('Digite o terceiro numero'))
+
+print ('Soma:', ((2*n1) * (n2/2)))
+print ('Produto:', (3 * n1) + n3)
+print ('Cubo:', n3**3)
